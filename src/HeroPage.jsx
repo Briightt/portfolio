f68118d35a259360e643ec28c461f83f2fd70d5d
Briightt <mustafaabdulkareem16@gmail.com>
2025-11-02 00:05:25 +0400

    Refactor HeroPage component structure and syntax

--- src/HeroPage.jsx
+++ src/HeroPage.jsx
@@ -8,10 +8,10 @@ export default function HeroPage ({slideChildContainer}) {
 return (<>
 
 
-<motion.div variants = {slideChildContainer} className="grid grid-rows-1 [grid-template-areas:'video'] will-change-transform will-change-opacity justify-center mb-15" >
-<motion.video variants = {slideChildContainer} playsInline muted autoPlay loop disablePictureInPicture controls={false} src= {video} className='w-full [grid-area:video] h-auto object-cover
+<div variants = {slideChildContainer} className="grid grid-rows-1 [grid-template-areas:'video'] will-change-transform will-change-opacity justify-center mb-15" >
+<video variants = {slideChildContainer} playsInline muted autoPlay loop disablePictureInPicture controls={false} src= {video} className='w-full [grid-area:video] h-auto object-cover
  ' >
-</motion.video>
+</video>
 <motion.div variants = {slideChildContainer} 
 className='[grid-area:video] z-[1] flex flex-col lg:flex-row flex-1 items-center justify-center lg:justify-evenly font-medium leading-snug p-3 lg:gap-15'>
 <motion.div variants = {slideChildContainer} className='flex flex-col gap-2 flex-wrap items-center lg:items-start'>
@@ -51,7 +51,7 @@ rounded-2xl bg-clip-padding p-0.5 bg-gradient-to-tr from-gray-950 to bg-orange-4
 </motion.img>
 </motion.div>
 </motion.div>
-</motion.div>
+</div>
 
 
 
@@ -68,6 +68,7 @@ rounded-2xl bg-clip-padding p-0.5 bg-gradient-to-tr from-gray-950 to bg-orange-4
 </>)
 
 }
+
 
 
 
