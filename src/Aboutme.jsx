48d00426ea111410a9d6ad243ec3e27cded2b66b
Mustafa abdul kareem <mustafaabdulkareem16@gmail.com>
2025-11-23 21:53:32 +0400

    Add files via upload

--- src/Aboutme.jsx
+++ src/Aboutme.jsx
@@ -50,10 +50,17 @@ years old born in Iraq, now living in UAE, Sharjah.
   started with off with HTML, CSS and JS, now I'm focusing on React.js and TypeScript, developed around 10 plus projects, 
   would say I'm confident enough to do problem solving in JS, however just began typeScript.js not too recently and overtime 
   I learned tailwind to up the speed of my styling and framer motion to animate websites.</p>
-  <motion.ul className='flex flex-row sm:max-w-4xl md:flex-nowrap lg:justify-start justify-center items-center gap-10 max-w-sm  mt-5 rounded-2xl sm:py-2 md:p-5 bg-[#97979711] mb-10 w-full h-full' whileHover={{translateY:-10}} transition={{duration:0.14, ease: 'easeInOut'}} variants = {slideChildContainer}>
-  <li className='font-medium sm:text-[0.9rem] text-[0.75rem] md:text-[1.1rem] text-orange-400' variants = {slideChildContainer}>Nationality: <span className='font-light text-[0.70rem] sm:text-[0.77rem] line-clamp-1 sm:line-clamp-none md:text-[1.05rem] text-[#ffffffc5]' variants = {slideChildContainer}>Iraq</span></li>
-  <li className='font-medium sm:text-[0.9rem] text-[0.75rem] md:text-[1.1rem] text-orange-400' variants = {slideChildContainer}>Location: <span className='font-light text-[0.70rem] sm:text-[0.77rem] line-clamp-1 sm:line-clamp-none md:text-[1.05rem] text-[#ffffffc5] ' variants = {slideChildContainer}>UAE, Sharjah</span></li>
-  <li className='font-medium sm:text-[0.9rem] text-[0.75rem] md:text-[1.1rem] text-orange-400' variants = {slideChildContainer}>Spoken Languages: <span className='font-light text-[0.70rem] sm:text-[0.77rem] line-clamp-1 sm:line-clamp-none md:text-[1.05rem] text-[#ffffffc5]' variants = {slideChildContainer}>English, Arabic</span></li>
+  <motion.ul className='flex flex-row sm:max-w-3xl md:flex-nowrap justify-center items-center gap-10 max-w-md 
+   mt-5 rounded-2xl sm:py-2 md:p-4 bg-[#97979711] mb-10 w-full h-full sm:leading-10 leading-8 ' whileHover={{translateY:-10}} transition={{duration:0.14, ease: 'easeInOut'}} variants = {slideChildContainer}>
+  <li className='font-medium sm:text-[0.9rem] text-[0.75rem] md:text-[1.1rem] text-orange-400 flex flex-col  md:gap-1 lg:flex-row items-center justify-center' variants = {slideChildContainer}>Nationality:
+     <span className='font-light text-[0.70rem] sm:text-[0.77rem]  md:text-[1.05rem] text-[#ffffffc5]' variants = {slideChildContainer}>Iraq</span></li>
+
+  <li className='font-medium sm:text-[0.9rem] text-[0.75rem] md:text-[1.1rem] text-orange-400 flex flex-col gap-1 lg:flex-row items-center'aaaa variants = {slideChildContainer}>Location:
+     <span className='font-light text-[0.70rem] sm:text-[0.77rem]  md:text-[1.05rem] text-[#ffffffc5]' variants = {slideChildContainer}>UAE, Sharjah</span></li>
+
+  <li className='font-medium sm:text-[0.9rem] text-[0.75rem] md:text-[1.1rem] text-orange-400 flex flex-col gap-1 lg:flex-row items-center' variants = {slideChildContainer}>Spoken Languages: 
+    <span className='font-light text-[0.70rem] sm:text-[0.77rem]  md:text-[1.05rem] text-[#ffffffc5]' variants = {slideChildContainer}>English, Arabic</span></li>
+
 </motion.ul>
 </motion.div>
 
@@ -75,6 +82,5 @@ variants = {slideChildContainer}>{hoverIndex === index && languages[index]}</mot
 </>)
 
 }
-
 
 
